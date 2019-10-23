802fa1419605fc554d9a76f6337686f29308ed74
Lara Wootton <larawootton@gmail.com>
2019-10-23 18:36:17 +0200

    wrangling done for now

--- src/Analysis/Prelim_ED_analysis.R
+++ src/Analysis/Prelim_ED_analysis.R
@@ -9,7 +9,7 @@ source(here("src/Data_wrangling", "Abundance_for_analysis.R"))
 glimpse(all_dat)
 
 #Get ED data
-ED_dat <- read.csv(here("Raw/Unprocessed", "ED_growth_metrics_23Oct_full.csv"), sep = ";" )
+ED_dat <- read.csv(here("Raw/Unprocessed", "ED_growth_metrics_23Oct_edited.csv"), sep = ";" )
 
 #Extract just the soils that were used in the experiment
 
@@ -52,6 +52,48 @@ plot(soils_field$Oophytum_sp ~ soils_field$Soil)
 
 #Wrangling ED data ####
 
-ED_dat <- ED_dat %>% filter(!notes == "wrong_id")
+ED_dat <- ED_dat %>% filter(!notes == "wrong_id") #Exclude misidentified individuals
 
+filtered_ED_dat <- ED_dat %>% filter(!(is.na(Height_1) &
+                                           is.na(Height_2) &
+                                           is.na(Height_3))) #Removing rows where there was never an individual, besides those pots where nothing sprouted
 
+#Replace NAs with 0 to show mortality
+filtered_ED_dat$Height_1[is.na(filtered_ED_dat$Height_1)] <- 0
+filtered_ED_dat$Height_2[is.na(filtered_ED_dat$Height_2)] <- 0
+filtered_ED_dat$Height_3[is.na(filtered_ED_dat$Height_3)] <- 0
+
+mean_ED_dat <- filtered_ED_dat %>% 
+                   group_by(Soil, Species) %>% 
+                   summarise(H1 = mean(Height_1),
+                             H2 = mean(Height_2),
+                             H3 = mean(Height_3))
+glimpse(mean_ED_dat)
+
+hist(mean_ED_dat$H1[mean_ED_dat$Species == "A"],
+     xlim = c(0,40),
+     breaks = seq(0,40,2))
+hist(mean_ED_dat$H2[mean_ED_dat$Species == "A"], 
+     add = T,
+     col = "grey",
+     xlim = c(0,40),
+     breaks = seq(0,40,2))
+hist(mean_ED_dat$H3[mean_ED_dat$Species == "A"], 
+     add = T,
+     col = "black",
+     xlim = c(0,40),
+     breaks = seq(0,40,2))
+
+hist(mean_ED_dat$H1[mean_ED_dat$Species == "B"],
+     xlim = c(0,15),
+     breaks = seq(0,40,1))
+hist(mean_ED_dat$H2[mean_ED_dat$Species == "B"], 
+     add = T,
+     col = "grey",
+     xlim = c(0,15),
+     breaks = seq(0,40,1))
+hist(mean_ED_dat$H3[mean_ED_dat$Species == "B"], 
+     add = T,
+     col = "black",
+     xlim = c(0,15),
+     breaks = seq(0,40,1))
