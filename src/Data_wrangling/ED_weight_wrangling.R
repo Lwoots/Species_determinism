b911efaf772d78e55f7cf6cc4722272b423d6f59
Lara Wootton <larawootton@gmail.com>
2020-02-02 14:27:58 +0200

    Converted epu to mm, and found avg excluding zeros

--- src/Data_wrangling/ED_weight_wrangling.R
+++ src/Data_wrangling/ED_weight_wrangling.R
@@ -32,11 +32,13 @@ raw_weights <- raw_weights[-363,]
 #Remove NAs
 raw_weights <- na.omit(raw_weights)
 
+
 #Average by code
 avg_mass <- raw_weights %>% group_by(Code) %>% summarise(Mass = mean(Mass))
 
 
-#Growth data ####
+
+#### Growth data ####
 
 #Get ED data
 
@@ -65,5 +67,43 @@ cleaned_ED_dat <- cleaned_ED_dat[-c(548:551),]
 cleaned_ED_dat$Individ[722] <- 5
 
 #Replace NAs with 0
-
 cleaned_ED_dat[is.na(cleaned_ED_dat)] <- 0
+
+#For species measured under the microscope, divide by 10 to convert epu to mm.
+
+cleaned_ED_dat[cleaned_ED_dat$Species == "G", c(8,10,12)] <- cleaned_ED_dat[cleaned_ED_dat$Species == "G", c(8,10,12)]/10
+cleaned_ED_dat[cleaned_ED_dat$Species == "H", c(8,10,12)] <- cleaned_ED_dat[cleaned_ED_dat$Species == "H", c(8,10,12)]/10
+cleaned_ED_dat[cleaned_ED_dat$Species == "I", c(8,10,12)] <- cleaned_ED_dat[cleaned_ED_dat$Species == "I", c(8,10,12)]/10
+
+#Rename
+
+names(cleaned_ED_dat)[1] <- "Code"
+
+
+#I want to average height by group, not including individuals of zero height.
+#Need to write a function for this
+zero_free_mean <- function(x) {
+    zvals <- x==0
+    if (all(zvals)) 0 else mean(x[!zvals])
+}
+
+#Test it
+
+#foo <- c(4, 5, 6)
+#goo <- c(4, 5, 6, 0)
+#hoo <- c(0, 0, 0)
+#
+#mean(foo)
+#mean(goo)
+#zero_free_mean(goo)
+#zero_free_mean(hoo)
+
+avg_height <- cleaned_ED_dat %>% group_by(Code) %>% summarise(Height_t1 = zero_free_mean(Height_1),
+                                                              Height_t2 = zero_free_mean(Height_2),
+                                                              Height_t3 = zero_free_mean(Height_3))
+
+
+#Combining datasets ####
+
+full_ED_data <- left_join(avg_height, avg_mass, by = "Code")
+plot(full_ED_data$Mass, full_ED_data$Height_t3)
